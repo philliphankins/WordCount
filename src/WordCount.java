5b9280db6aa698985978c528537b1d5d97ad69fd
philliphankins <34249287+philliphankins@users.noreply.github.com>
2018-01-15 16:12:33 -0700

    Initial Commit

---
+++ src/WordCount.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("tailoftwocities.txt");
+        Scanner scanner = new Scanner(file);
+
+
+
+    }
+}
